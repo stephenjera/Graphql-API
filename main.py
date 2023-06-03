d3b49b9aad79bd13511b9190588e063ccaecb82d
stephenjera <stephen.411@hotmail.co.uk>
2023-06-03 07:52:07 +0100

    Testing querying dataset

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-import sys
 from fastapi import FastAPI
+from google.cloud import bigquery
 
 app = FastAPI()
 
+# Create a BigQuery client
+client = bigquery.Client()
+
 @app.get("/")
 async def root():
-    python_version = sys.version
-    return {"message": f"Hello, World! Running on Python {python_version}"}
+    # Define your BigQuery project and dataset IDs
+    PROJECT_ID = "graphql-land"
+    DATASET_ID = "football"
+
+    # Define the BigQuery table name
+    TABLE_NAME = f"{PROJECT_ID}.{DATASET_ID}.matches"
+
+    # Query example
+    query = f"""
+     SELECT *
+     FROM `{TABLE_NAME}`
+    """
+    query_job = client.query(query)
+
+    # Iterate over the query results
+    result = []
+    for row in query_job:
+        result.append(dict(row))
+
+    return result
