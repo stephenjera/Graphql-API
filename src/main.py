d975650ec06ed72e613a207d08e819f7dd298d8e
stephenjera <stephen.411@hotmail.co.uk>
2023-06-04 18:24:17 +0100

    Added cors to allow fetch requests

--- src/main.py
+++ src/main.py
@@ -5,3 +5,15 @@ from src.schema import schema
 
 app = FastAPI()
 app.include_router(GraphQLRouter(schema=schema, path="/graphql"))
+
+origins = [
+    "https://super-portfolio-lyart.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
